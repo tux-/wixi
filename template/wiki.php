9f4bbb9f29e302bcc191299b1104715e5c28dcf9
Tux Solbakk <tux@krageroby.no>
2024-07-08 20:38:44 +0000

    Ability to change page slug

--- template/wiki.php
+++ template/wiki.php
@@ -2,11 +2,29 @@
 declare(strict_types=1);
 namespace gimle;
 
+$slug = page('page');
+if ($slug === null) {
+	$slug = '';
+}
+
 ?>
 <div class="content">
 	<div id="editorjs">
 	</div>
 </div>
+<dialog id="postmenu" popover="auto">
+	<div class="close">
+		<button>x</button>
+	</div>
+	<h1>Postmenu</h1>
+	<div class="forms">
+		<form id="movepost" action="<?=BASE_PATH?>movepost" method="post">
+			<input type="hidden" name="from" value="<?=htmlspecialchars($slug)?>"/>
+			<input type="text" name="slug" value="<?=htmlspecialchars($slug)?>"/>
+			<button>Move</button>
+		</form>
+	</div>
+</dialog>
 <?php
 
 return true;
